dfdc263ca471ef381768f59323030ff08f9fdf75
Spike <lukasz.piwowar1@protonmail.com>
2021-09-24 20:17:55 +0200

    added /questions endpoint returning 10 random questions

--- backend/ssiema.js
+++ backend/ssiema.js
@@ -3,7 +3,8 @@ let connection = mysql.createConnection({
   host: 's148.cyber-folks.pl',
   user: 'goethe_eden-faceden',
   password: '#-k^g%IaoK-AS5q2',
-  database: 'goethe_eden-faceden'
+  database: 'goethe_eden-faceden',
+  multipleStatements: true
 });
 
 connection.connect(function(err) {
@@ -13,37 +14,58 @@ connection.connect(function(err) {
 });
 
 function getQuestions() {
-    let selectQueryRand = 'SELECT * FROM ?? WHERE is_random = 0';
-    let selectQueryNoRand = 'SELECT * FROM ?? WHERE is_random = 1';    
+    const selectQuery = 'SELECT * FROM ?? WHERE ? = ?';    
 
-    let queryRand = mysql.format(selectQueryRand,["questions"]);
-    let queryNoRand = mysql.format(selectQueryNoRand,["questions"]);
+    const queryRand = mysql.format(selectQuery, ["questions", "is_random", 0]);
+    const queryNoRand = mysql.format(selectQuery, ["questions", "is_random", 1]);
 
-    const selectedQuestions = [];
 
-    connection.query(queryRand,(err, data) => {
+    connection.query(`${queryRand};${queryNoRand}`,(err, results) => {
         if(err) return console.log(err);
     
+        const selectedQuestions = [];
+        const dataRand = results[0];
+        const dataNoRand = results[1];
+        let chosenIndexes = [];
 
-        for (let i = 0; i < 5; i++) {
-            const randNum = Math.floor(Math.random() * data.length);
-            selectedQuestions.push([data[randNum].id, data[randNum].content]);
-            data.splice(randNum, 1);
+        for (let i = 0; i < 5; ++i) {
+            const randNum = Math.floor(Math.random() * dataRand.length);
+            if (chosenIndexes.includes(randNum)) {
+                --i;
+                continue;
+            }
+            selectedQuestions
+                .push({"id": dataRand[randNum].id, "content": dataRand[randNum].content});
+            chosenIndexes.push(randNum);
         }
-    });
 
-    connection.query(queryNoRand,(err, data) => {
-        if(err) return console.log(err);
-    
 
-        for (let i = 0; i < 5; i++) {
-            const randNum = Math.floor(Math.random() * data.length);
-            selectedQuestions.push([data[randNum].id, data[randNum].content]);
-            data.splice(randNum, 1);
+        for (let i = 0; i < 5; ++i) {
+            const randNum = Math.floor(Math.random() * dataNoRand.length);
+            if (chosenIndexes.includes(randNum)) {
+                --i;
+                continue;
+            }
+            selectedQuestions
+                .push({"id": dataNoRand[randNum].id, "content": dataNoRand[randNum].content});
+            chosenIndexes.push(randNum);
         }
+
         console.log(selectedQuestions);
+        fyShuffle(selectedQuestions);
+        console.log(selectedQuestions);
+        connection.end();
     });
-    connection.end();
 };
+
+function fyShuffle(a) {
+    if (a.length < 2) return;
+    for (var i = a.length; --i >= 1; ) {
+     var j = ~~(Math.random() * (i + 1)), tmp;
+     tmp = a[j];
+     a[j] = a[i];
+     a[i] = tmp;
+    }
+  }
 
 getQuestions();
