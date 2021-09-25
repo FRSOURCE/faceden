9a9d2fdaabf494fe4b2748306fb3ea9cf9fc2776
Dominik Kapusta <60074519+Doggnito@users.noreply.github.com>
2021-09-25 16:41:07 +0200

    Feat:  chosenteam

--- client/src/components/ChosenTeamView.vue
+++ client/src/components/ChosenTeamView.vue
@@ -1,20 +1,57 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'ChosenTeamView'
-})
-</script>
-
 <template>
   <div :class="$style.container">
-    <h2 :class="$style.h2">Gratulacje zostałeś przydzielony do ...!</h2>
+    <h2 :class="$style.h2">Gratulacje! Zostałeś przydzielony do <span :class="$style['c-green']">"{{ drawedTeam.name }}"</span>!</h2>
     <div :class="$style['image-box']"> 
-      <img src="@/assets/images/009-turtle.svg" alt="Logo" :class="$style.image">
+      <component :is="svgName" :class="$style.image" />
     </div>
     <h2 :class="$style.h2">Zapamiętaj swoją drużynę!</h2>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref } from 'vue'
+import Unicorn from '@/assets/images/1.svg'
+import Ghost from '@/assets/images/2.svg'
+import Dragon from '@/assets/images/3.svg'
+import Narhval from '@/assets/images/4.svg'
+import Mushroom from '@/assets/images/5.svg'
+import Octopus from '@/assets/images/6.svg'
+import Pigeon from '@/assets/images/7.svg'
+import Turtle from '@/assets/images/8.svg'
+
+export default defineComponent({
+  name: 'ChosenTeamView',
+  components: {
+    Unicorn,
+    Ghost,
+    Dragon,
+    Narhval,
+    Mushroom,
+    Octopus,
+    Pigeon,
+    Turtle
+  },
+  setup() {
+    const localStorageTeam: string | null = localStorage.getItem('registerTeam')
+    const drawedTeam: Ref<{id: number, name: string} | {}> = ref({})
+    const images = ['Unicorn', 'Ghost', 'Dragon', 'Narhval', 'Mushroom', 'Octopus', 'Pigeon', 'Turtle']
+
+    if (localStorageTeam) {
+      drawedTeam.value = JSON.parse(localStorageTeam)
+    }
+
+    const svgName = ref('Unicorn')
+    if (drawedTeam.value) {
+      svgName.value = images[drawedTeam.value.id - 1]
+    }
+
+    return {
+      svgName,
+      drawedTeam
+    }
+  }
+})
+</script>
 
 <style module lang="scss">
   .container {
@@ -26,18 +63,22 @@ export default defineComponent({
 
   .h2 {
     font-size: 25px;
-    margin: 60px;
+    margin: 60px 15px;
     text-align: center;
     color: #fff;
   }
 
   .image {
-    width: 50vw;
+    width: 40vw;
     padding: 50px;
   }
 
   .image-box {
     border: thin solid #0eff6e;
     border-radius: 50%;
+  }
+
+  .c-green {
+    color: #0eff6e;
   }
 </style>
--- client/src/components/StartingScreen.vue
+++ client/src/components/StartingScreen.vue
@@ -1,3 +1,11 @@
+<template>
+  <div :class="$style.container">
+    <h1 :class="$style.h1">Faceden</h1>
+    <p :class="$style.text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt elit at turpis lacinia, ac sodales magna rutrum. Fusce mollis maximus nisl vitae pharetra. Nulla nec tincidunt nisi. Sed et orci diam. Aenean efficitur justo neque, commodo convallis magna auctor sit amet. Nunc molestie consequat nisl, ut tristique purus iaculis et. Aliquam ornare justo a lectus porta, nec ultrices neque pellentesque. Phasellus vel erat egestas, egestas augue vitae, aliquam erat.</p>
+    <button type="button" :class="$style['button-run-form']" @click="navigateTo('')">Zaczynamy!</button>
+  </div>
+</template>
+
 <script lang="ts">
 import { defineComponent } from 'vue'
 
@@ -12,14 +20,6 @@ export default defineComponent({
 })
 </script>
 
-<template>
-  <div :class="$style.container">
-    <h1 :class="$style.h1">Faceden</h1>
-    <p :class="$style.text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt elit at turpis lacinia, ac sodales magna rutrum. Fusce mollis maximus nisl vitae pharetra. Nulla nec tincidunt nisi. Sed et orci diam. Aenean efficitur justo neque, commodo convallis magna auctor sit amet. Nunc molestie consequat nisl, ut tristique purus iaculis et. Aliquam ornare justo a lectus porta, nec ultrices neque pellentesque. Phasellus vel erat egestas, egestas augue vitae, aliquam erat.</p>
-    <button type="button" :class="$style['button-run-form']" @click="navigateTo('ChosenTeamView')">Zaczynamy!</button>
-  </div>
-</template>
-
 <style module lang="scss">
   .container {
     display: flex;
@@ -29,20 +29,22 @@ export default defineComponent({
   }
 
   .h1 {
-    font-size: 48px;
+    font-size: 60px;
     margin: 60px;
+    font-family: 'Libre Caslon Text', serif;
   }
 
   .text {
-    padding: 30px;
+    padding: 15px 30px;
     line-height: 1.5rem;
     text-align: center;
     color: #fff;
   }
 
   .button-run-form {
-    padding: 20px;
-    font-size: 24px;
+    padding: 15px;
+    margin: 15px;
+    font-size: 32px;
     border: thin solid #000;
     cursor: pointer;
     color: #fff;
