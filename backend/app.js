8c86b0f9fbbae561f8be9ce1744730990dfccae5
Filip232 <filip.wesolowski@onet.eu>
2021-09-25 15:16:20 +0200

    Form post (#6)
    feat: form submit first changes
    
    chore: questions refactor
    
    connection with db
    
    Co-authored-by: Spike <lukasz.piwowar1@protonmail.com>

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 
 const questionsRouter = require('./routes/questions');
+const indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/questions', questionsRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
